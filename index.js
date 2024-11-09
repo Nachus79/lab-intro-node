e504320e16cb78141594d5f23664ba48a7ea507d
Nacho Monzón <ignaciomonzona@gmail.com>
2024-11-09 11:09:15 +0100

    iteration 5 complete

--- index.js
+++ index.js
@@ -1,13 +1,66 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
 
-  add(item) {}
+      this.items = [];
+      this.length = 0;  
 
-  get(pos) {}
 
-  max() {}
+  }
 
-  min() {}
+  add(item) {
+
+    this.items.push(item); //Para introducir desde el final.
+    this.items.sort((a, b) => a - b); //Ordena en orden ascendente.
+    this.length = this.items.length; //Revisa la nueva longitud.
+  }
+
+  get(pos) {
+
+    const valueSearch = this.items[pos];
+
+    if (!valueSearch){
+
+      throw new Error("OutOfBounds");
+    } 
+
+    return valueSearch;
+
+    
+  }
+
+  max() {
+
+      if (this.length === 0) {
+
+        throw new Error("EmptySortedList"); 
+
+      }
+
+      return this.items[this.length - 1]; //Devuelve el más grande si el array está ordenado. 
+
+      /*Si no está ordenado: 
+
+
+
+      */
+
+  }
+
+  min() {
+
+    if (this.length === 0) {
+
+      throw new Error("EmptySortedList"); 
+
+    }
+
+     return this.items[0]; //Si está ordenado el más bajo es el primero. 
+
+     /*Si no lo está: 
+     
+     */
+
+  }
 
   sum() {}
 
@@ -15,3 +68,12 @@ class SortedList {
 }
 
 module.exports = SortedList;
+
+
+
+/*
+Iteration 5: min()
+The min() method should return the lowest value of the array.
+
+In case you have an empty SortedList, you must throw an error with the message "Empty SortedList".
+*/
